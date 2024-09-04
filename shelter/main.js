27a86572ca88981e332dfb4ff98147e7b78025ab
kristina <krisbubenka@icloud.com>
2024-09-04 11:04:04 +0300

    fix base burger menu and add burger menu to "our-pets" page

--- shelter/main.js
+++ shelter/main.js
@@ -2,7 +2,13 @@
 document.addEventListener("DOMContentLoaded", function () {
   const burgerBtn = document.getElementById("burger-btn");
   const appHeader = document.querySelector("header");
+  const setLinks = document.querySelector(
+    ".app-header__components__set-of-links"
+  );
   burgerBtn.addEventListener("click", function () {
+    appHeader.classList.toggle("open");
+  });
+  setLinks.addEventListener("click", function () {
     appHeader.classList.toggle("open");
   });
 });

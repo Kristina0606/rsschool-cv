1c46035e060c65c982f8fb136b231bfb89f68e85
kristina <krisbubenka@icloud.com>
2024-09-04 12:41:23 +0300

    add burger menu and fix it

--- shelter/main.js
+++ shelter/main.js
@@ -13,7 +13,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
   setLinks.addEventListener("click", function (event) {
     event.stopPropagation();
-    appHeader.classList.remove("open");
+    appHeader.classList.toggle("open");
   });
 });
 

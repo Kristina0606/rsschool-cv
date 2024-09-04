587d11181ceba58f3552d9c1156a053ee1a94983
kristina <krisbubenka@icloud.com>
2024-09-04 12:38:16 +0300

    fix burger

--- shelter/main.js
+++ shelter/main.js
@@ -5,10 +5,26 @@ document.addEventListener("DOMContentLoaded", function () {
   const setLinks = document.querySelector(
     ".app-header__components__set-of-links"
   );
-  burgerBtn.addEventListener("click", function () {
+
+  burgerBtn.addEventListener("click", function (event) {
+    event.stopPropagation();
     appHeader.classList.toggle("open");
   });
-  setLinks.addEventListener("click", function () {
-    appHeader.classList.toggle("open");
+
+  setLinks.addEventListener("click", function (event) {
+    event.stopPropagation();
+    appHeader.classList.remove("open");
   });
+});
+
+document.addEventListener("click", (e) => {
+  const appHeader = document.querySelector("header");
+  const setLinks = document.querySelector(
+    ".app-header__components__set-of-links"
+  );
+  const withinBoundaries = e.composedPath().includes(setLinks);
+
+  if (!withinBoundaries && appHeader.classList.contains("open")) {
+    appHeader.classList.toggle("open");
+  }
 });
